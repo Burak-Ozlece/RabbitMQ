c4e8e649e0a01ec760a0082aabbf7e367bc3c514
Burak Özlece <burakozleceplus@gmail.com>
2023-04-29 18:51:04 +0300

    5. Video bitti

--- RabbitMQ.Consumer/Consumer/Program.cs
+++ RabbitMQ.Consumer/Consumer/Program.cs
@@ -14,17 +14,23 @@ IConnection connection = factory.CreateConnection();
 IModel channel = connection.CreateModel();
 
 // Queu oluşturma
-channel.QueueDeclare(queuName, exclusive: false); //Diğer tarafda nasıl bir yapılandırma varsa aynısı yapılmalı
+channel.QueueDeclare(queuName, exclusive: false,durable:true); //Diğer tarafda nasıl bir yapılandırma varsa aynısı yapılmalı
 // Queue'dan mesaj okuma
 EventingBasicConsumer consumer = new(channel); //Event tanımlamamız gerek
-channel.BasicConsume(queuName, false, consumer); // autoAck: kutruktan alınan mesajı kurukta silip silinmemesi
+var consumerTag = channel.BasicConsume(queuName, false, consumer); // autoAck: kutruktan alınan mesajı kurukta silip silinmemesi
+channel.BasicQos(0, 1, false);
 
 consumer.Received += (sender, e) =>
 {
     //Kuyruğa gelen mesajların işlendiği yer.
     //e.Body : Kuyrukdaki mesajın verisini bütünsel olarak getirecektir.
-    //e.Body.Span / e.Body.ToArray() : Kuyrukdaki mesajın byte verisini getirecektir.
+    //e.Body.Span veya e.Body.ToArray() : Kuyrukdaki mesajın byte verisini getirecektir.
     var message = Encoding.UTF8.GetString(e.Body.Span); //byte[] türünden gelen mesajı stringe çeviriyoruz
+    channel.BasicAck(e.DeliveryTag, false); // multiple false değeri vererek sadece bu mesaj için cevap döndürüyoruz.
+
+    //channel.BasicCancel(consumerTag); // Gelen tüm mesajları bu şekilde red edilebiliyor
+
+    //channel.BasicReject(3, true); // Tek bir mesajın işlenmesini reddetmek
     Console.WriteLine(message); 
 };
 
--- RabbitMQ.Publisher/Publisher/Program.cs
+++ RabbitMQ.Publisher/Publisher/Program.cs
@@ -12,17 +12,21 @@ factory.Uri = new("amqps://jzmftdtu:3y8Mi9FFRYDQlIuUTOHVEF-xDxUKsvoX@hawk.rmq.cl
 using IModel channel = connection.CreateModel();
 
 //Queue oluşturma
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+//channel.QueueDeclare(queue: "example-queue", exclusive: false);
+channel.QueueDeclare(queue: "example-queue", exclusive: false,durable:true);
 // queue: kuyruğun adı, durable: kuyrukdaki mesajların kalıcılığı ile alakalı, exclusive: bu kuyruğun özel olup olmaması, yani bu bağlantının dışında başka bir bağlantı bağlanmıyor true olduğunda,, ayriyetden bağlantı kapandığı zaman o kuyruk imha edilir, autoDelete: kuyruğun içindeki tüm mesajlar tüketildiğinde, kuyruğun silinip silinemiyeceğini talimat sağlar.
 
 // Queue'ya mesaj gönderme
+
+IBasicProperties properties = channel.CreateBasicProperties(); //Mesajları kalıcı olması için
+properties.Persistent = true; //Mesajları kalıcı olması için
 
 //RabbitMQ kuruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte dönüştürmemiz gerekir.
 for (int i = 0; i < 100; i++)
 {
     byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
     await Task.Delay(200);
-    channel.BasicPublish(exchange: string.Empty, routingKey: "example-queue", body: message); // Exchange belirtmediğimiz zaman direct exhange davranış benimser ve bu exchange varsayılandır.
+    channel.BasicPublish(exchange: string.Empty, routingKey: "example-queue", body: message, basicProperties: properties); // Exchange belirtmediğimiz zaman direct exhange davranış benimser ve bu exchange varsayılandır.
 }
 
 

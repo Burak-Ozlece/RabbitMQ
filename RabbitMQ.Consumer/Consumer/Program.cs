5ee2968404f5957fb547301caef67625c5948626
Burak Özlece <burakozleceplus@gmail.com>
2023-04-29 16:51:19 +0300

    İlk kodalar ve notlar

---
+++ RabbitMQ.Consumer/Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿// Bağlantı oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var queuName = "example-queue";
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://jzmftdtu:3y8Mi9FFRYDQlIuUTOHVEF-xDxUKsvoX@hawk.rmq.cloudamqp.com/jzmftdtu");
+
+// Bağlantı aktifleştirma
+IConnection connection = factory.CreateConnection();
+
+// Kanal oluşturma
+IModel channel = connection.CreateModel();
+
+// Queu oluşturma
+channel.QueueDeclare(queuName, exclusive: false); //Diğer tarafda nasıl bir yapılandırma varsa aynısı yapılmalı
+// Queue'dan mesaj okuma
+EventingBasicConsumer consumer = new(channel); //Event tanımlamamız gerek
+channel.BasicConsume(queuName, false, consumer); // autoAck: kutruktan alınan mesajı kurukta silip silinmemesi
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajların işlendiği yer.
+    //e.Body : Kuyrukdaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span / e.Body.ToArray() : Kuyrukdaki mesajın byte verisini getirecektir.
+    var message = Encoding.UTF8.GetString(e.Body.Span); //byte[] türünden gelen mesajı stringe çeviriyoruz
+    Console.WriteLine(message); 
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new(); //İlk önce ConnectionFactory den bir instance oluşturuy
+factory.Uri = new("amqps://jzmftdtu:3y8Mi9FFRYDQlIuUTOHVEF-xDxUKsvoX@hawk.rmq.cloudamqp.com/jzmftdtu");
+
+//Bağlantıyı aktifleştirme
+ using IConnection connection = factory.CreateConnection();
+
+// Kanal oluşturuyoruz
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+// queue: kuyruğun adı, durable: kuyrukdaki mesajların kalıcılığı ile alakalı, exclusive: bu kuyruğun özel olup olmaması, yani bu bağlantının dışında başka bir bağlantı bağlanmıyor true olduğunda,, ayriyetden bağlantı kapandığı zaman o kuyruk imha edilir, autoDelete: kuyruğun içindeki tüm mesajlar tüketildiğinde, kuyruğun silinip silinemiyeceğini talimat sağlar.
+
+// Queue'ya mesaj gönderme
+
+//RabbitMQ kuruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte dönüştürmemiz gerekir.
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    await Task.Delay(200);
+    channel.BasicPublish(exchange: string.Empty, routingKey: "example-queue", body: message); // Exchange belirtmediğimiz zaman direct exhange davranış benimser ve bu exchange varsayılandır.
+}
+
+
+Console.Read();

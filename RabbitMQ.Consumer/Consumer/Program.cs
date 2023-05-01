1213b677c3c6da100be0523b42a64be516aa35e2
Burak Özlece <burakozleceplus@gmail.com>
2023-05-01 18:51:51 +0300

    Fanout Exchange bitti

--- RabbitMQ.Consumer/Consumer/Program.cs
+++ RabbitMQ.Consumer/Consumer/Program.cs
@@ -3,29 +3,29 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
-var queuName = "example-queue";
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://jzmftdtu:3y8Mi9FFRYDQlIuUTOHVEF-xDxUKsvoX@hawk.rmq.cloudamqp.com/jzmftdtu");
-
-// Bağlantı aktifleştirma
 IConnection connection = factory.CreateConnection();
-
-// Kanal oluşturma
 IModel channel = connection.CreateModel();
 
-// Queu oluşturma
-channel.QueueDeclare(queuName, exclusive: false); //Diğer tarafda nasıl bir yapılandırma varsa aynısı yapılmalı
-// Queue'dan mesaj okuma
-EventingBasicConsumer consumer = new(channel); //Event tanımlamamız gerek
-channel.BasicConsume(queuName, false, consumer); // autoAck: kutruktan alınan mesajı kurukta silip silinmemesi
+channel.ExchangeDeclare("Burak-fanout-exchange", type: ExchangeType.Fanout); // Publisher tarafında tanımladığımız exchange'cin aynısı burda tanımlıyoruz
+
+Console.Write("Kuyruk adı giriniz: "); // Kullanıcıdan roting key adı girmesini istiyoruz
+
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(queueName,exclusive:false); // Kuruğu tanımlıyoruz
+
+channel.QueueBind(queueName, "Burak-fanout-exchange", string.Empty); // Kuyruğu exchange'ce bind ediyoruz
+
+EventingBasicConsumer consumer = new(channel); // Consumer örneği oluşturuyoruz
+channel.BasicConsume(queueName, false, consumer); // BasicConsume tanımlıyoruz
 
 consumer.Received += (sender, e) =>
 {
-    //Kuyruğa gelen mesajların işlendiği yer.
-    //e.Body : Kuyrukdaki mesajın verisini bütünsel olarak getirecektir.
-    //e.Body.Span / e.Body.ToArray() : Kuyrukdaki mesajın byte verisini getirecektir.
-    var message = Encoding.UTF8.GetString(e.Body.Span); //byte[] türünden gelen mesajı stringe çeviriyoruz
-    Console.WriteLine(message); 
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+    channel.BasicAck(e.DeliveryTag, false); // Başarılı bir şekilde okunan mesajı, silmesi için DeliveryTag'ini geri gönderiyoruz
 };
 
 Console.Read();
--- RabbitMQ.Publisher/Publisher/Program.cs
+++ RabbitMQ.Publisher/Publisher/Program.cs
@@ -2,27 +2,18 @@
 using System.Text;
 
 //Bağlantı oluşturma
-ConnectionFactory factory = new(); //İlk önce ConnectionFactory den bir instance oluşturuy
+ConnectionFactory factory = new();
 factory.Uri = new("amqps://jzmftdtu:3y8Mi9FFRYDQlIuUTOHVEF-xDxUKsvoX@hawk.rmq.cloudamqp.com/jzmftdtu");
-
-//Bağlantıyı aktifleştirme
  using IConnection connection = factory.CreateConnection();
-
-// Kanal oluşturuyoruz
 using IModel channel = connection.CreateModel();
 
-//Queue oluşturma
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
-// queue: kuyruğun adı, durable: kuyrukdaki mesajların kalıcılığı ile alakalı, exclusive: bu kuyruğun özel olup olmaması, yani bu bağlantının dışında başka bir bağlantı bağlanmıyor true olduğunda,, ayriyetden bağlantı kapandığı zaman o kuyruk imha edilir, autoDelete: kuyruğun içindeki tüm mesajlar tüketildiğinde, kuyruğun silinip silinemiyeceğini talimat sağlar.
+channel.ExchangeDeclare("Burak-fanout-exchange", ExchangeType.Fanout); // Fanout exchange tanımlıyoruz
 
-// Queue'ya mesaj gönderme
-
-//RabbitMQ kuruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte dönüştürmemiz gerekir.
-for (int i = 0; i < 100; i++)
+for (int i = 1; i <= 100; i++)
 {
-    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
     await Task.Delay(200);
-    channel.BasicPublish(exchange: string.Empty, routingKey: "example-queue", body: message); // Exchange belirtmediğimiz zaman direct exhange davranış benimser ve bu exchange varsayılandır.
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    channel.BasicPublish("Burak-fanout-exchange", string.Empty, body: message); // Fanout exchange routing key'e bakmadan gönderdiği için, routing key'i boş geçiyoruz
 }
 
 
